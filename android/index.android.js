9475c78b61530e1569280ebaa87d7cbd68da6d87
James Nocentini <james.nocentini@gmail.com>
2015-11-25 09:03:59 +0000

    change filestructure

--- android/index.android.js
+++ android/index.android.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var React = require('react-native');
-var Home = require('./app/components/Home');
+var Home = require('./src/components/Home');
 var {
   AppRegistry,
   StyleSheet,
